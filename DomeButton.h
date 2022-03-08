bd07d0a553e1d8546df67703e4a91d4ec0014700
Doc Snyder <44336447+R2ToolDroid@users.noreply.github.com>
2022-03-08 13:50:30 +0100

    Back to Road

--- DomeButton.h
+++ DomeButton.h
@@ -1,18 +1,77 @@
-Button BtnL(A0);       // define the button
-Button BtnR(A1);       // define the button
 
-//ezButton buttonL(A0);  // create ezButton object
-//ezButton buttonR(A1);  // create ezButton object
+byte BtnR_UP true;
+byte BtnL_UP true;
 
-DomeButton(){
-  
-    BtnL.read();               // read the button
+byte BtnR = false;
+byte BtnL = false;
+byte BtnBooth = false;
+int mode = 0;
+unsigned long BtnTA, BtnTB;
+const long trig = 100;
 
-    if (BtnL.wasReleased())    // if the button was released, change the LED state
-    {
-         Serial.println("  --  released "); 
+
+void CheckBtnState(){
+
+
+
+    if ( (analogRead(DOME_BTN_L) <= 60) && (analogRead(DOME_BTN_R) <= 60)){
+      Serial.println(" Button Booth pressed");
+      BtnBooth = true;
+
+
+    } else {
+      BtnBooth = false;
     }
-   
-   
-  
-  }
+
+    if (analogRead(DOME_BTN_R) <= 60){
+      Serial.println(" Button Right pressed");
+      BtnR = true;
+
+    } else {
+      BtnR = false;
+    }
+
+    if (analogRead(DOME_BTN_L) <= 60){
+      Serial.println(" Button Left pressed");
+      BtnR = true;
+
+    } else {
+      BtnR = false;
+    }
+
+
+
+}
+
+void DomeButton(){
+
+  BtnTA = millis();
+
+   if (BtnTA - BtnTB >= trig) {
+
+      BtnTB = BtnTA;
+      CheckBtnState();
+
+      if (BtnR){
+        mode++;
+        Serial.print("Mode= ");
+        Serial.println(mode);
+      }
+
+      if (BtnL){
+        Serial.println("---EXECUTE----");
+
+      }
+
+      if (BtnBooth){
+        Serial.print(" - RESET - ");
+        Serial.print("Mode= ");
+        Serial.println(mode);
+        mode = 0;
+      }
+      //Serial.print(analogRead(DOME_BTN_R));
+      //Serial.print(" - ");
+      //Serial.println(analogRead(DOME_BTN_L));
+
+      } //delay
+}
--- PenumBra-Dome.ino
+++ PenumBra-Dome.ino
@@ -14,31 +14,22 @@
  SV5   CS
  SV6   CLK
  SV7   DATA
-
  --- Logic Engine ---
-
  DOUT 7  REAR
  DOUT 8  FRONT 
-
  --- HoloLights ---
-
  DOUT 1   FRONT
  DOUT 2   REAR
  DOUT 3   TOP
   
-
-
  --- Holo Servos ---
-
  PCA 1:::
-
  --- Dome Button --
-
  Left  Analog1   A0
  Right Analog2   A1
   
  */
-#include <JC_Button.h>          // https://github.com/JChristensen/JC_Button
+
 #include "ReelTwo.h"
 #include "core/Animation.h"
 #include "core/DelayCall.h"
@@ -54,20 +45,19 @@
 #define COMMAND_SERIAL Serial //   Serial1 for LIVE 
 
 #define PSI_COM Serial3 //  serial for PSI Pro
+#define DOME_BTN_L A0
+#define DOME_BTN_R A1
 
-//#define DOME_BTN_L A0
-//#define DOME_BTN_R A1
- 
 
 #define HOLO_HSERVO        0x1000
 #define HOLO_VSERVO        0x2000
 
 #define HOLO_SERVOS_MASK        (HOLO_HSERVO|HOLO_VSERVO)
- 
+
 const ServoSettings servoSettings[] PROGMEM = {
 
     //* PIN, closed 0, Open 1, GRoupe//
-    
+
     { 1, 800, 1200, HOLO_HSERVO },  /* 0: horizontal front holo */
     { 2, 800, 1200, HOLO_VSERVO },  /* 1: vertical front holo */
     { 3, 800, 1200, HOLO_HSERVO },  /* 2: horizontal top holo */
@@ -85,7 +75,7 @@ AnimationPlayer player(servoSequencer);
 JawaCommander<> jawaCommander;
 MarcduinoSerial<> marcduinoSerial(COMMAND_SERIAL, player);
 
- 
+
 HoloLights frontHolo(22,HoloLights::kRGB, HoloLights::kFrontHolo,12);        // PIN Dout1
 HoloLights rearHolo(23, HoloLights::kRGB, HoloLights::kRearHolo);         // PIN Dout2
 HoloLights topHolo(24, HoloLights::kRGB, HoloLights::kTopHolo);          // PIN Dout3
@@ -115,23 +105,23 @@ void resetSequence()
         "HPA000|0\n"
         "CB00000\n"
         "DP00000\n"));
-      
+
       //CommandEvent::process(F("HPA000|0"));
 }
 
- 
+
 #include "MarduinoHolo.h"
 #include "MarcduinoSequence.h"
 #include "MarcduinoMagicPanel.h"
 
 #include "DomeButton.h"
 
- 
+
 void setup()
 {
-     BtnL.begin();              // initialize the button object
-     BtnR.begin();              // initialize the button object
-   
+    pinMode(DOME_BTN_L, INPUT_PULLUP);
+    pinMode(DOME_BTN_R, INPUT_PULLUP);
+
     REELTWO_READY();
     Wire.begin();
     //Wire.setClock(400000); //Set i2c frequency to 400 kHz.
@@ -142,23 +132,23 @@ void setup()
     servoDispatch.setClockCalibration((const uint32_t[]) { 27570000, 27190000 });
     //servoDispatch.setServosEasingMethod(HOLO_SERVOS_MASK, Easing::CircularEaseIn);
 
-    
+
     frontHolo.assignServos(&servoDispatch, 0, 1);
     topHolo.assignServos(&servoDispatch, 2, 3);
     rearHolo.assignServos(&servoDispatch, 4, 5);
 
-   
-    
+
+
     COMMAND_SERIAL.begin(9600);
     PSI_COM.begin(2400);
     SetupEvent::ready();
-    
+
     DEBUG_PRINTLN("ready.."); 
 
-  
+
     FLD.selectScrollTextLeft("R2\n    D2", LogicEngineRenderer::kBlue, 1, 15);
     RLD.selectScrollTextLeft("... RSeries LogicEngine ....", LogicEngineRenderer::kYellow, 0, 15);
-    
+
     //CommandEvent::process(F("HPF104"));  
     //servoDispatch.moveTo(0, 150, 1000, 1.0);  
     CommandEvent::process(F("HPA199"));  //Twitch for all
@@ -168,7 +158,7 @@ void setup()
 
     //PSI_COM.print("0T2\r");
 
-    
+
 /*  Magic Panel Commands:
 kNormal   
 kSolid  
@@ -184,9 +174,7 @@ kExpandHollow
 kCollapseHollow   
 kForwardQ   
 kReverseQ 
-
 #### PSI PRO Commands : ###
-
 Mode 0 - Turn Panel off (This will also turn stop the Teeces if they share the serial connection and the "0" address is used)
 Mode 1 - Default (Swipe) The default mode can be changed on the config.h tab
 Mode 2 - Flash (fast flash) (4 seconds) Use caution around those sensitive to flashing lights.
@@ -210,20 +198,17 @@ Mode 19 - Light Saber Battle
 Mode 20 - Star Wars Intro (scrolling yellow "text" getting smaller and dimmer)
 Mode 21 - VU Meter (4 seconds)
 Mode 92 - VU Meter -Runs Indefinitely (Spectrum on Teeces)
-
-
-
 */
-    
+
 
 }  /*END SETUP */
 
 
- 
+
 void loop()
 {
     AnimatedEvent::process();
     DomeButton();
-   
-    
+
+
 }
